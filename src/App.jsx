0eecfd8afba9e247b015d7c58ec6096b1cfdfa5b
Sarah Wiseman <s.wiseman777@gmail.com>
2024-04-25 11:49:16 +0100

    cookies per second upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import "./app.css";
 export default function App() {
   const [cookies, setCookies] = useState(0);
 
@@ -8,22 +8,59 @@ export default function App() {
   useEffect(() => {
     // a timer to be created when the page loads to increase cookies by cps every second
     const myInterval = setInterval(() => {
-      increaseCookies();
-    }, 1000);
-    // function to clean up my timer when I rerun the useEffect
-    return clearInterval(myInterval);
-  }, []);
+      increaseCookiesByOne();
+    }, 1000 / cookiesPerSec);
 
-  function increaseCookies() {
-    setCookies((currentCookies) => currentCookies + 1);
+    // function to clean up my timer when I rerun the useEffect
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [cookiesPerSec]);
+
+  function increaseCookiesByOne() {
+    console.log("cookie clicker clicked!");
+    setCookies((currentCookies) => {
+      return currentCookies + 1;
+    });
   }
+
+  function buyUpgrade() {
+    setCookiesPerSec(cookiesPerSec + 1);
+  }
+
+  function tenCookies() {
+    setCookiesPerSec(cookiesPerSec + 10);
+    setCookies((currentCookies) => {
+      return currentCookies - 10;
+    });
+  }
+
+  function hundredCookies() {
+    setCookiesPerSec(cookiesPerSec + 100);
+    setCookies((currentCookies) => {
+      return currentCookies - 100;
+    });
+  }
+  function thousandCookies() {
+    setCookiesPerSec(cookiesPerSec + 1000);
+    setCookies((currentCookies) => {
+      return currentCookies - 1000;
+    });
+  }
+
   return (
-    <div>
+    <div id="container">
       <h1>Cookie Clicker</h1>
-      <button onClick={increaseCookies}>I am a cookie...</button>
+      <button onClick={increaseCookiesByOne}>I am a cookie...</button>
+      {/* <button onClick={buyUpgrade}>Get +1 cookies per second!</button> */}
+      <button onClick={tenCookies}>Get +10 cookies per second!</button>
+      <button onClick={hundredCookies}>Get +100 cookies per second!</button>
+      <button onClick={thousandCookies}>Get +1000 cookies per second!</button>
+
       {/* image onclick... */}
       {/* <img onClick={}  src="" alt="" />  */}
       <p>I have {cookies} cookies.</p>
+      <p>Collecting {cookiesPerSec} cookies per second.</p>
     </div>
   );
 }
